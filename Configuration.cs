90e44f9d05fdfce4cee35cf4460178c50f516d5a
Cenaril <cenarilffxiv@gmail.com>
2025-10-06 19:43:07 +0200

    Initial commit - StepCounter plugin

---
+++ Configuration.cs
@@ -0,0 +1,28 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+
+namespace StepCounter;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 1;
+
+    public int TotalSteps { get; set; } = 0;
+    public bool IsConfigWindowMovable { get; set; } = true;
+    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+
+    [NonSerialized]
+    private IDalamudPluginInterface? pluginInterface;
+
+    public void Initialize(IDalamudPluginInterface pluginInterface)
+    {
+        this.pluginInterface = pluginInterface;
+    }
+
+    public void Save()
+    {
+        pluginInterface?.SavePluginConfig(this);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,81 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using StepCounter.Windows;
+
+namespace StepCounter;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+    [PluginService] internal static IChatGui ChatGui { get; private set; } = null!; // Aggiunto
+
+    private const string CommandName = "/step";
+
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("StepCounter");
+    private ConfigWindow ConfigWindow { get; init; }
+    private MainWindow MainWindow { get; init; }
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+
+        Configuration.Initialize(PluginInterface);
+
+        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+
+        ConfigWindow = new ConfigWindow(this);
+        MainWindow = new MainWindow(this, goatImagePath);
+
+        WindowSystem.AddWindow(ConfigWindow);
+        WindowSystem.AddWindow(MainWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Increase or show the step counter"
+        });
+
+        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
+        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUi;
+
+        Log.Information($"[{PluginInterface.Manifest.Name}] inizializzato correttamente.");
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        Configuration.TotalSteps++;
+        Configuration.Save();
+
+        ChatGui.Print($"You did {Configuration.TotalSteps} total steps!");
+
+        MainWindow.Toggle(); // Mostra la finestra principale
+    }
+
+    public void ToggleConfigUi() => ConfigWindow.Toggle();
+    public void ToggleMainUi() => MainWindow.Toggle();
+
+    public void Dispose()
+    {
+        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
+        PluginInterface.UiBuilder.OpenMainUi -= ToggleMainUi;
+
+        WindowSystem.RemoveAllWindows();
+        ConfigWindow.Dispose();
+        MainWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+}
+
